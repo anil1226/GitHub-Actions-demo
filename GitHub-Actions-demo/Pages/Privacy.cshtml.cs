7bd5e963b784cb4ca09cf87aab00ec44f805faed
Anil Karuturi <anilchowdary1226@gmail.com>
2023-05-03 12:01:46 +0530

    Add project files.

---
+++ GitHub-Actions-demo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitHub_Actions_demo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
